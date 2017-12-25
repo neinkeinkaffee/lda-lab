c0462ba0098d367c91e97b3b04dfc150a0aec4ca
Gesa Stupperich <gesa.stupperich@gmail.com>
2017-12-25 20:49:35 +0100

    Refactor javascript as a first step towards code reuse

--- src/main/resources/static/js/words.js
+++ src/main/resources/static/js/words.js
@@ -21,24 +21,7 @@ var lda_url = "http://localhost:8080/corpus/" + corpusName + "/numberOfTopics/" 
 var lda_api_url = "http://localhost:8080/api/corpus/" + corpusName + "/numberOfTopics/" + numberOfTopics + "/timestamp/" + timestamp;
 var topic_url = lda_api_url + "/topic/" + topicId;
 
-function showMoreTopicWords(tbody, data, max, n) {
-
-    var currentLength = tbody.selectAll("tr").size();
-
-    var rows = tbody.selectAll("tr")
-        .data(data.wordProbabilities.slice(0, currentLength + n))
-        .enter()
-        .append("tr");
-
-    rows.append("td")
-        .append("a")
-        .attr("href", function (row) {
-            return lda_url + "/word/" + row['lemma'];
-        })
-        .text(function (row) {
-            return row['lemma'];
-        });
-
+function appendProbabilities(rows, max) {
     rows.append("td")
         .text(function (row) {
             return row['probability'];
@@ -57,12 +40,46 @@ function showMoreTopicWords(tbody, data, max, n) {
         });
 }
 
-function showLessTopicWords(tbody, data, n) {
+function appendLinks(rows, key, value) {
+    rows.append("td")
+        .append("a")
+        .attr("href", function (row) {
+            return lda_url + "/" + key + "/" + row[value];
+        })
+        .text(function (row) {
+            return row['lemma'];
+        });
+}
+
+function showMore(tbody, data, max, n, key, value) {
     var currentLength = tbody.selectAll("tr").size();
     var rows = tbody.selectAll("tr")
-        .data(data.wordProbabilities.slice(0, currentLength - n))
+        .data(data.slice(0, currentLength + n))
+        .enter()
+        .append("tr");
+    appendLinks(rows, key, value);
+    appendProbabilities(rows, max);
+}
+
+function showLess(tbody, data, n) {
+    var currentLength = tbody.selectAll("tr").size();
+    var rows = tbody.selectAll("tr")
+        .data(data.slice(0, currentLength - n))
         .exit()
         .remove();
+}
+
+function buildTable(data, tableName, key, value) {
+    var table = d3.select("#" + tableName + "Table");
+    var tbody = table.append("tbody");
+    var max = data[0]['probability'];
+    showMore(tbody, data, max, 10, key, value);
+    document.getElementById(tableName + "Less").onclick = function () {
+        showLess(tbody, data, 5);
+    }
+    document.getElementById(tableName + "More").onclick = function () {
+        showMore(tbody, data, max, 5, key, value);
+    }
 }
 
 d3.request(topic_url)
@@ -71,19 +88,7 @@ d3.request(topic_url)
         return JSON.parse(xhr.responseText);
     })
     .get(function (data) {
-        var table = d3.select("#topicWordsTable");
-        var tbody = table.append("tbody");
-
-        var max = data.wordProbabilities[0]['probability'];
-
-        showMoreTopicWords(tbody, data, max, 10);
-
-        document.getElementById("topicWordsLess").onclick = function() {
-            showLessTopicWords(tbody, data, 5);
-        }
-        document.getElementById("topicWordsMore").onclick = function() {
-            showMoreTopicWords(tbody, data, max, 5);
-        }
+        buildTable(data.wordProbabilities, "topicWords", "word", "lemma");
     });
 
 
