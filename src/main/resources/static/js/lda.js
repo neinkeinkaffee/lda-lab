d6e621fdfca2776100739557ec307b76038224b4
Gesa Stupperich <gesa.stupperich@gmail.com>
2017-12-29 22:00:52 +0100

    Change trigger element for expanding and collapsing

--- src/main/resources/static/js/lda.js
+++ src/main/resources/static/js/lda.js
@@ -1,19 +1,19 @@
-d3.selectAll(".expandButton")
+d3.selectAll(".expandVolumeTitle")
     .on("click", function() {
         var volume = this.id.split("_")[1];
         d3.select("#volume_" + volume)
             .style("display", "block");
-        d3.select("#collapseButton_" + volume)
+        d3.select("#collapseVolumeTitle_" + volume)
             .style("display", "block");
         d3.select(this)
             .style("display", "none");
     });
-d3.selectAll(".collapseButton")
+d3.selectAll(".collapseVolumeTitle")
     .on("click", function() {
         var volume = this.id.split("_")[1];
         d3.select("#volume_" + volume)
             .style("display", "none");
-        d3.select("#expandButton_" + volume)
+        d3.select("#expandVolumeTitle_" + volume)
             .style("display", "block");
         d3.select(this)
             .style("display", "none");
